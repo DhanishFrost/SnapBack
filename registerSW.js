cf186339dba6adad48773a0262f6ca78240dc294
DhanishFrost <cb009562@students.apiit.lk>
2023-09-12 21:17:28 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/SnapBack/sw.js', { scope: '/SnapBack/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
