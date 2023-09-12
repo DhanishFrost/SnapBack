4757c7c438db71be413b6b18c6b7839b20f4f240
DhanishFrost <cb009562@students.apiit.lk>
2023-09-12 21:33:49 +0530

    vite update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/SnapBack/',
+  base: './',
   plugins: [
     react(),
     VitePWA({
@@ -13,7 +13,7 @@ export default defineConfig({
         short_name: 'SnapBack',
         description: 'A gaming web application',
         start_url: '/SnapBack/',
-        scope: '/SnapBack/',
+        scope: './',
         orientation: 'portrait',
         display: 'standalone',
         background_color: '#ffffff',
