678fc5efe740449b72b718dea31c9dc0303b8434
DhanishFrost <cb009562@students.apiit.lk>
2023-09-12 20:51:02 +0530

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -41,11 +41,6 @@ export default defineConfig({
             "type": "image/png"
           },
           {
-            "src": "/icons/icon-144x144.png",
-            "sizes": "144x144",
-            "type": "image/png"
-          },
-          {
             "src": "/icons/icon-192x192.png",
             "sizes": "192x192",
             "type": "image/png"
